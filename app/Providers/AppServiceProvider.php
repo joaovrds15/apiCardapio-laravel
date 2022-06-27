eef3526c189a4591b86e9eeebeba7ff907c1c358
joao-victor <joaovictorodrigues4009@gmail.com>
2022-06-27 13:39:13 -0300

    changes app service

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -4,6 +4,8 @@ declare(strict_types=1);
 
 namespace App\Providers;
 
+use Illuminate\Support\Facades\App;
+use Illuminate\Support\Facades\URL;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -25,6 +27,8 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        //
+        if (!App::environment('local')) {
+            URL::forceSchema('https');
+       }
     }
 }
