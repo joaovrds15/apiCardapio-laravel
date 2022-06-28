3240c8840d3bc3c30ff37073804dedb4e5e318ad
joao-victor <joaovictorodrigues4009@gmail.com>
2022-06-28 10:28:07 -0300

    removing dd

--- app/Http/Controllers/ApiFilesController.php
+++ app/Http/Controllers/ApiFilesController.php
@@ -60,8 +60,6 @@ class ApiFilesController extends Controller
         $storageBucketName = config('googlecloud.storage_bucket');
         $bucket = $storage->bucket($storageBucketName);
         $filePath = $this->uploadServer($request);
-        dd($filePath);
-        /*
         $fileSource = fopen($filePath, 'r');
         $newFolderName = 'images';
         $googleCloudStoragePath = $newFolderName.'/'.basename($filePath);
@@ -72,7 +70,7 @@ class ApiFilesController extends Controller
         return response()->json([
             "google_storage_url" => 'https://storage.cloud.google.com/'.$storageBucketName.'/'.$googleCloudStoragePath,
         ]);
-        */
+        
     }
     
     public static function deleteStorage($ImageURL){
